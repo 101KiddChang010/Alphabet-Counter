479440d7b0188235809514859a49747e5b12a86b
KiddChang02 <91109377+KiddChang02@users.noreply.github.com>
2022-03-04 21:02:31 -0500

    Add files via upload

---
+++ File reader - alphabet counter.py
@@ -0,0 +1,53 @@
+import string
+
+def lowerAlphabet(): 
+    return string.ascii_lowercase
+
+def arrayAlphabet(a): 
+    return list(a)
+
+def readLetters(d):
+    lengDict = len(d)
+    lower = lowerAlphabet()
+
+    inc = {}
+
+    for letter in lower:
+        inc[letter] = 0
+
+    for x in range(lengDict):
+        for letter in d[x]:
+            for low in lower:
+                if letter == low:
+                    inc[letter] += 1 
+                else:
+                    pass
+
+    return inc
+
+def histogram(d):
+    print("letter    count")
+    
+    letter = arrayAlphabet(lowerAlphabet())
+    value = readLetters(d)
+
+    for x in range(26): 
+        print("{:10}{}".format(letter[x], value[letter[x]]))
+
+def main():
+    file = input("Enter file name: ")
+    f = open(file, "r")
+
+    s = {}
+    x = int(0)
+
+    #   splits every word in file into a dictionary
+    for line in f:
+        L = line.split()
+        for key in L:
+            s[x] = key.lower()
+            x += 1
+
+    histogram(s)
+
+    f.close()
